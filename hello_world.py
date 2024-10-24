18df4d28105f424366264375e3d91545b5c39b86
victorj61 <167463378+victorj61@users.noreply.github.com>
2024-10-24 03:12:09 -0400

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,133 @@
 print("Hello World!")
+print("I'm here")
+print('Whatever "here" means')
+print('Hello, World!') 
+print("I'm studying Python")
+print('I\m studying Python')
+print('I\'m studying Python', 'and having fun!')
+print('I\'m studying\nPython')
+greeting = 'Hello, friend!'
+print('greeting')
+
+greeting = 'Hello, friend!'
+print(greeting)
+
+greeting = 'Hi, everybody!'
+print(greeting)
+
+_correctName
+correctName
+correctName89
+
+
+greeting
+Greeting
+
+greeting = 'Hello, friend!'
+age = 35
+speed1 = 4.5
+speed2 = 4.0
+speed3 = 4.
+speed4 = 4
+
+am_i_ugly = False
+
+print('sample') # here is a mid-line comment
+
+# note that the varibales are strings, not numbers
+var1 = '23'
+var2 = '54'
+
+#scientific notation
+#3e4 = 3e4 = 3 * 10000 = 30000
+#3e-4 = 3e-4 =3 *1/10000 = 0.0003
+#3e-4 = 3e-4=3 * 1/10000 = 0.0003
+print(0.00000000000000000000000000003)
+
+#octal numbers
+#start with 0O or 0o
+print(0o123)
+
+#hexiademical numbers
+# start with 0X or 0x
+print(0x123)
+
+2 + 3
+5 - 4
+3 * 5
+
+# standard division
+6 / 2
+7 / 2
+
+#integer division
+6 // 2
+7 // 2
+
+#modulus division
+6 % 2
+7 % 2
+
+5 / 0 # = error b/c you cant divide by 0
+
+#power operator
+2 ** 3 = 8
+
+
+print('What is your name')
+user_name = input()
+print('Hello,' user_name)
+
+login = input ('Enter your login')
+language = input('Enter your native language')
+
+print('Your login is', login, 'and you speak', language)
+
+# Type Casting
+# Float numbers - numbers that have a decimal point
+
+height_cm = input('Height converter: enter your height in cm: ')
+print('Your height in feet is:', height_cm / 30.48)
+
+height_cm = input('Height converter: enter your height in cm: ')
+float_height_cm = float(height_cm)
+print('Your height in feet is:', float_height_cm / 30.48)
+
+#or
+
+height_cm = float (input('Height converter: enter your height in cm: '))
+print('Your height in feet is:', height_cm / 30.48)
+
+year_born = int(input('What year were you born?'))
+print('In 2100, you will be', 2100 - year_born, 'years old, provided you live this long!')
+
+temp_c = input('Enter the temperature today in Celsius degrees: ')
+temp_f = float(temp_c) * 1.8 + 32
+temp_Statement = str(temp_c) + ' degrees Celsius equals ' + str(temp_f) + ' degrees Fahrenheit '
+print(temp_statement)
+
+#Coding Exercise 4
+
+hours = float(input('How many hours did you work last month? '))
+rate = float(input('What is your hourly rate '))
+
+print('Ladt month, you earned', hours * rate, 'dollars')
+ 
+# Order of pemdas applies EXCEPT for when operation is the same
+# Then you read right to left
+# ** means to exponent the number
+
+len('Hello!') = 6
+print('Hello, World!' end='.')
+print('Python speaking!')
+Hello, World!.Python speaking!
+
+first_name = 'John'
+print('Your first name is', first_name, 'Welcome!')
+
+Your first name is John Welcome! 
+# A space was automatically put in
+
+first_name = 'John'
+print('Your first name is', first_name, 'Welcome!', sep='-')
+Your first name is-John-Welcome! 
